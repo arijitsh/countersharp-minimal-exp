dd1a7e1eda64f261d048ebb19c95bdbc632fe462
Arijit Shaw <if.arijit@gmail.com>
2022-01-14 18:31:38 +0100

    name files as smt2

--- run-all.sh
+++ run-all.sh
@@ -3,6 +3,6 @@
 echo "Writing logs to ./results/..."
 
 run-counterSharp instances.csv
-run-approxmc instances.csv
-run-ganak instances.csv
-run-dimovski instances-dimovski.csv
+#run-approxmc instances.csv
+#run-ganak instances.csv
+#run-dimovski instances-dimovski.csv
